cceeaca57fc9e59b6cee775541e125f44667beb5
Eike Wulff <e.wulff@aleron.eu>
2017-07-15 15:01:03 +0200

    Whatsappmessage gebaut

---
+++ YouHaveBeenHacked/Assets/Script/SendMessage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SendMessage : MonoBehaviour
+{
+	public InputField InputField;
+	public RectTransform panel;
+	public RectTransform prefab;
+	public ScrollRect scrollView;
+	public void SendStuff()
+	{
+		var newMessage = Instantiate(prefab);
+		var setup = newMessage.GetComponent<Setup>();
+		setup.layout = Setup.ImagePosition.Left;
+		setup.Text = InputField.text;
+		newMessage.transform.SetParent(panel.transform,false);
+		Canvas.ForceUpdateCanvases ();
+		scrollView.verticalScrollbar.value=0f;
+		Canvas.ForceUpdateCanvases ();
+	}
+}
