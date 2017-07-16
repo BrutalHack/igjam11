d473295960e82961aa446ff50a233539117833c1
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2017-07-16 09:40:36 +0200

    Extenden Paket and Letter with functionality.

--- YouHaveBeenHacked/Assets/Script/Manager/LetterManager.cs
+++ YouHaveBeenHacked/Assets/Script/Manager/LetterManager.cs
@@ -1,6 +1,28 @@
-﻿    public class LetterManager :Manager
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LetterManager : Manager
+{
+    public Animator Letter;
+    public Image LetterContent;
+    
+    public override void HandleNewState(State state)
     {
-        public override void HandleNewState(State state)
-        {
-        }
+        Letter.gameObject.SetActive(true);
+        Letter.SetTrigger("Open");
     }
+
+    public void OpenLetter()
+    {
+        Debug.Log("Open Letter.");
+        Letter.gameObject.SetActive(false);
+        LetterContent.gameObject.SetActive(true);
+    }
+
+    public void CloseLetter()
+    {
+        Debug.Log("Close Letter.");
+        LetterContent.gameObject.SetActive(false);
+        SetNextState();
+    }
+}
--- YouHaveBeenHacked/Assets/Script/Manager/PaketManager.cs
+++ YouHaveBeenHacked/Assets/Script/Manager/PaketManager.cs
@@ -1,6 +1,28 @@
-﻿public class PaketManager : Manager
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PaketManager : Manager
 {
+    public Animator Paket;
+    public Image PaketContent;
+    
     public override void HandleNewState(State state)
     {
+        Paket.gameObject.SetActive(true);
+        Paket.SetTrigger("Open");
+    }
+
+    public void OpenPaket()
+    {
+        Debug.Log("Open Paket.");
+        Paket.gameObject.SetActive(false);
+        PaketContent.gameObject.SetActive(true);
+    }
+
+    public void ClosePaket()
+    {
+        Debug.Log("Close Paket.");
+        PaketContent.gameObject.SetActive(false);
+        SetNextState();
     }
 }
