4eb30f17b83ae8b0424c23d5dabc5e0dd6d4949a
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2017-07-16 11:49:46 +0200

    Rotation script and adjustment of spinner png.

---
+++ YouHaveBeenHacked/Assets/Script/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+	private float _speed = 160f;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.back * Time.deltaTime * _speed);
+	}
+}
